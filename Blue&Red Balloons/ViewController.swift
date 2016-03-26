62c887e2d81e9dbdcc2bc7395e1a5627dff3a4de
Neeru <Raghav@raghavs-imac.local>
2016-03-26 10:39:01 +0530

    Initial Commit

---
+++ Blue&Red Balloons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Blue&Red Balloons
+//
+//  Created by Raghav Vashisht on 26/03/16.
+//  Copyright © 2016 Raghav Vashisht. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
